fe33f76cb8f9536665b57a365be200fb98722b8b
Devko <61373609+DevkoDev@users.noreply.github.com>
2024-08-14 20:10:22 +0300

    upload

---
+++ t.js
@@ -0,0 +1,54 @@
+//The token you want to forward messages from
+let usertoken = "ODMzM-FlA34T0";
+//The channels you want to forward from (channel IDs)
+
+let forwardfrom = ['953605123282702386'];
+//The channels you want to forward to (channel IDs)
+let forwardto = ['https://discord.com/api/webhooks/943542138027794462/'];
+const {
+    Webhook,
+    MessageBuilder 
+} = require('discord-webhook-node');
+
+const Discord = require("discord.js-selfbot-v11");
+const user = new Discord.Client();
+user.login(usertoken);
+user.on("ready", ready => {
+    user.on("message", message => {
+        if (forwardfrom.includes(message.channel.id)) {
+            forwardto.forEach(channel => {
+                let hook = new Webhook(channel);
+                hook.setAvatar("https://cdn.discordapp.com/avatars/"+message.author.id+"/"+message.author.avatar+".jpeg")
+                hook.setUsername(message.author.username)
+                if(message.content.length > 1){
+                hook.send(message.content);}
+            });
+            //forward all attachments
+            
+            if (typeof message.attachments.array()[0] != "undefined") {
+                var media = message.attachments.array();
+                let urls = [];
+                media.forEach(item => {
+                    urls.push(item.url);
+                });
+                forwardto.forEach(channel => {
+                    let hook = new Webhook(channel);
+                    hook.setAvatar("https://cdn.discordapp.com/avatars/"+message.author.id+"/"+message.author.avatar+".jpeg")
+                    hook.setUsername(message.author.username)
+                    hook.send(message.author.tag + "\nImage URLs:\n" + urls.join("\n"));
+                });
+            }
+            //forward all embeds
+            message.embeds.forEach(embed1 => {
+                let embedfinal = new Discord.RichEmbed(embed1);
+                console.log(embedfinal);
+                forwardto.forEach(channel => {
+                    let hook = new Webhook(channel);
+                    hook.setAvatar("https://cdn.discordapp.com/avatars/"+message.author.id+"/"+message.author.avatar+".jpeg")
+                    hook.setUsername(message.author.username)
+                    hook.send(embedfinal);
+                });
+            });
+        }
+    });
+});
